640352210dab7ec2da964348806cc74a95148fe5
taha uygun <tahauygun@gmail.com>
2020-03-17 23:20:46 -0700

    adding style for remove todo

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -4,6 +4,7 @@
         <div class="todos">
             <div v-for="todo in allTodos" :key="todo.id" class="todo">
                 {{todo.title}}
+                <i class="fas fa-trash-alt"></i>
             </div>
         </div>
     </div>
@@ -35,6 +36,13 @@
         border-radius: 5px;
         text-align: center;
         position: relative;
+        cursor: pointer;
+    }
+    i{
+        position: absolute;
+        bottom: 10px;
+        right: 10px;
+        color: #fff;
         cursor: pointer;
     }
 </style>
