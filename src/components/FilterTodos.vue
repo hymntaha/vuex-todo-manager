92ce22826473e4e6ba4715e5559ef2febde5fbc0
taha uygun <tahauygun@gmail.com>
2020-03-19 23:05:19 -0700

    setting up filter todos scaffold

---
+++ src/components/FilterTodos.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        Filter Todos:
+    </div>
+    <select>
+        <options value="200">200</options>
+        <options value="100">100</options>
+        <options value="50">50</options>
+        <options value="20">20</options>
+        <options value="10">10</options>
+        <options value="5">5</options>
+    </select>
+</template>
+<script>
+    export default {
+
+    }
+</script>
+<style>
+
+</style>
