d12631821386e6b2b5eecb512260810f44988277
taha uygun <tahauygun@gmail.com>
2020-03-21 23:22:41 -0700

    fetching limited response

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -24,7 +24,10 @@ const actions = {
     commit('removeTodo', id);
   },
   async filterTodos({commit},e){
-    console.log(e)
+
+    const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText);
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`)
+
   }
 
 };
